9a414bcc0c788d38993b9848c51c049c49212161
Ludovic Raess <ludovic.rass@gmail.com>
2024-02-29 17:42:01 +0100

    WIP

--- scripts/PW1D_noacc_T.jl
+++ scripts/PW1D_noacc_T.jl
@@ -2,6 +2,8 @@ using Plots, ElasticArrays, Printf
 
 @views avx(A) = (A[1:end-1] .+ A[2:end]) .* 0.5
 
+# switch_advect = WENA_5()!!!
+
 @views function main()
     # independent physics
     lc      = 1.0 #sqrt(k_r / ηf_r * η_r / ϕ_bg) # m
@@ -23,36 +25,40 @@ using Plots, ElasticArrays, Printf
     w       = w_lc * lc
     k_ηf0   = lc^2 / η_ϕbg
     λ       = 1.0
-    dt      = 1e-3 * tsc
+    dt      = 1e-3 * tsc / 20
     # numerics
     nx      = 200
-    nt      = 50
+    nt      = 1000
     maxiter = 100nx
     ϵtol    = [1e-5 1.0]
-    ncheck  = ceil(Int, 2nx)
+    ncheck  = ceil(Int, 40nx)
     # preprocessing
     dx      = lx / nx
     xc      = LinRange(-lx / 2 - dx / 2, lx / 2 + dx / 2, nx)
-    dτ_T    = dx^2 / λ / 3.1
     # init
     ϕ       = ϕ_bg .+ ϕA .* exp.(.-((xc .+ lx / 3) ./ w) .^ 2)
     T       = T_bg .+ TA .* exp.(.-((xc .+ lx / 3) ./ w) .^ 2)
+    # T = T_bg .* ones(nx)
+    # T[-lx / 2.8 .< xc .< -lx / 3.2] .= T_bg .+ TA
     ϕ_i     = copy(ϕ)
+    ϕ_old   = copy(ϕ)
+    T_old   = copy(T)
     Pe      = zeros(nx)
     qD      = zeros(nx - 1)
     η_ϕ     = zeros(nx)
     k_ηf    = zeros(nx)
+    dϕdt    = zeros(nx)
+    dTdt    = zeros(nx)
     RPe     = zeros(nx - 2)
     dτ_β    = zeros(nx - 2)
-    qUx     = zeros(nx - 1)
-    Vs      = zeros(nx - 1)
-    RT      = zeros(nx - 2)
-    Uf      = ones(nx)
-    Us      = ones(nx)
-    Ut_o    = (Uf .* ϕ + Us .* (1 .- ϕ))
+    qTx     = zeros(nx - 1)
+    # ρCp_f * ϕ * T + ρCp_s * (1-ϕ) * T -> ρCp_t * T
+    sum_T_i = sum(T)
     # action
     for it = 1:nt
         @printf("it = %d\n", it)
+        ϕ_old .= ϕ
+        T_old .= T
         iter = 1
         errs = 2.0 .* ϵtol
         errs_evo = ElasticArray{Float64}(undef, 2, 0)
@@ -66,33 +72,49 @@ using Plots, ElasticArrays, Printf
             RPe  .= .-Pe[2:end-1] ./ η_ϕ[2:end-1] .+ diff(qD) ./ dx
             dτ_β .= dx^2 ./ k_ηf[2:end-1] ./ 3.1
             Pe[2:end-1] .+= RPe .* dτ_β
+            @. dϕdt = -Pe / η_ϕ
+            @. ϕ = ϕ_old + dt * dϕdt
+
+            # d(ρcp_f * ϕ * T)/dt + d(ρcp_s * (1-ϕ) * T)/dt + ∇(ρcp_f * ϕ * T * Vf) + ∇(ρcp_s * (1-ϕ) * T * Vs) - ∇(λtot * ∇T) = 0
+
+            # T * d(ρcp_f * ϕ)/dt     + (ρcp_f * ϕ) * d(T)/dt +
+            # T * d(ρcp_s * (1-ϕ))/dt + (ρcp_s * (1-ϕ)) * d(T)/dt +
+            # T * ∇(ρcp_f * ϕ * Vf)     + ρcp_f * ϕ * Vf * ∇(T) +
+            # T * ∇(ρcp_s * (1-ϕ) * Vs) + ρcp_s * (1-ϕ) * Vs * ∇(T) - ∇(λtot * ∇T) = 0
+
+            # T * (d(ρcp_f * ϕ)/dt + d(ρcp_s * (1-ϕ))/dt + ∇(ρcp_f * ϕ * Vf) + ∇(ρcp_s * (1-ϕ) * Vs)) + # -> 0
+            # d(T)/dt * ((ρcp_f * ϕ) + (ρcp_s * (1-ϕ))) + # -> d(T)/dt * ρcp_t
+            # ρcp_f * qD * ∇(T) + ρcp_t * Vs * ∇(T) - ∇(λtot * ∇T) = 0
+
+            # D(T)/Dt * ρcp_t + # -> D(T)/Dt -> dT/dt + Vs * ∇(T)
+            # ρcp_f * qD * ∇(T) - ∇(λtot * ∇T) = 0
+
+            # D(T)/Dt + ρcp_f /  ρcp_t * qD * ∇(T) - λtot /  ρcp_t * ∇(∇T) = 0
+
             # energy / temperature
-            qUx  .= (avx(Uf) .* avx(ϕ) .* qD .+ avx(Us) .* (1 .- avx(ϕ)) .* Vs .- λ .* diff(T) ./ dx)
-            RT   .= .-((Uf[2:end-1] .* ϕ[2:end-1] + Us[2:end-1] .* (1 .- ϕ[2:end-1])) .- Ut_o[2:end-1]) ./ dt .- diff(qUx) ./ dx
-            T[2:end-1] .+= dτ_T .* RT
+            ret = 1e1
+            qTx .= .-λ .* diff(T) ./ dx
+            dTdt[2:end-1] .= .-diff(qTx) ./ dx
+            dTdt[1:end-1] .-= ret .* min.(qD, 0.0) .* diff(T) ./ dx
+            dTdt[2:end]   .-= ret .* max.(qD, 0.0) .* diff(T) ./ dx
+            T .= T_old .+ dt .* dTdt
             if iter % ncheck == 0
                 errs[1] = maximum(abs.(RPe))
-                errs[2] = maximum(abs.(RT))
+                errs[2] = (sum(T) .- sum_T_i) ./ sum_T_i
                 append!(errs_evo, errs)
                 push!(iters_evo, iter / nx)
                 @printf("  iter = %d, iter/nx = %1.3e, err = [ %1.3e %1.3e ] \n", iter, iter / nx, errs...)
             end
             iter += 1
         end
-        ϕ   .-= dt * (1 .- ϕ) .* Pe ./ η_ϕ
-        cumsum!(Vs, .-Pe[2:end] ./ η_ϕ[2:end]) .* dx
-        Ut_o .= (Uf .* ϕ + Us .* (1 .- ϕ))
+        if it % 10 == 0
         # visualisation
         p1 = plot([ϕ_i, ϕ], xc; title="Porosity", ylabel="depth", label=["Init" "φ"])
         p2 = plot(Pe, xc; title="Effective Pressure", label="Pe")
-        p3 = plot(Vs, xc[2:end]; title="Vs", label="Vs")
-        p4 = plot(T, xc; title="temperature", label="T")
-        # p4 = plot(iters_evo, err_evo'; title="Residual evolution", yaxis=:log10, marker=:circle, xlabel="Iter/nx", label="RPe")
-        display(plot(p1, p2, p3, p4; layout=(1, 4)))
+        p3 = plot(T, xc; xlabel="$(round(errs[2], sigdigits=4))", title="Temperature", label="T")
+        display(plot(p1, p2, p3; layout=(1, 3)))
+        end
     end
 end
 
 main()
-
-# doc
-# Pe = Pe + RPe./βf_dτ # explicit update
